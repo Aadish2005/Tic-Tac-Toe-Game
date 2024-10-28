bf3ec0a0d045488ac93b974ff0b183c65531fb49
Aadish Jain <118098474+Aadish2005@users.noreply.github.com>
2024-10-28 15:47:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+let buttons = document.querySelectorAll(".box");
+let resetgame = document.querySelector(".reset");
+let msg = document.querySelector("#msg");
+let msgcontainerdiv = document.querySelector(".msgcontainerdiv");
+let newgamebutton = document.querySelector(".newgamebutton");
+let theme = "black";
+let gamewon = false;
+resetgame.onclick = () => {
+    gamewon = false;
+    msgcontainerdiv.classList.add("hide");
+    buttons.forEach((button) => {
+        button.innerText = "";
+        button.disabled = false;
+    })
+    
+}
+// for (let index = 0; index < buttons.length; index++) {
+//     buttons[index].
+buttons.forEach((button) => 
+    {
+        button.onclick = () => {
+        if (gamewon) {
+            return;
+        }
+        if (theme == "black") {
+            button.innerText = "X";
+            theme = "white";
+        }
+        else {
+            button.innerText = "O";
+            theme = "black"
+        }
+        button.disabled = true;
+      
+          let result  = checkwinner();
+            if (result) {
+                gamewon= true;
+                show_winner(result);
+                
+           
+        }
+    }});
+ 
+    function checkwinner() {
+        let checkpatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for (let index = 0; index < checkpatterns.length; index++) {
+        let pattern = checkpatterns[index];
+        
+        if (buttons[pattern[0]].innerText === buttons[pattern[1]].innerText && buttons[pattern[0]].innerText === buttons[pattern[2]].innerText  && buttons[pattern[0]].innerText != "") {
+          
+            return buttons[pattern[0]].innerText + "  is Winner";
+        }
+    }
+    return false;
+    
+}
+ function show_winner ( winner)  {
+   msg.innerText = winner;
+   msgcontainerdiv.classList.remove("hide");
+}
